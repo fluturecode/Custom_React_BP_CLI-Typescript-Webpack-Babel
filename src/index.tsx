39224160dcd57461a4637f6a5fc932462d7f0981
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-08 21:55:25 -0500

    set up webpack.config.js

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+import * as ReactDom from "react-dom"
+
+const App = () => {
+	return <div> A fluturecode web production </div>
+}
+
+ReactDom.render(<App />, document.getElementById("root"))
