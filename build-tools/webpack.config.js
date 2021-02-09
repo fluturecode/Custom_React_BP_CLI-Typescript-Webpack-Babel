2a553eac1e3780f1126088f8ffcfacc2af2e5379
Elizabeth Eidelson <fluturecode@gmail.com>
2021-02-08 22:03:13 -0500

    fixed bug

--- build-tools/webpack.config.js
+++ build-tools/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 		vendor: ["react", "react-dom"],
 	},
 	output: {
-		path: path.resolve(_dirname, "dist"),
+		path: path.resolve(__dirname, "dist"),
 		filename: "js/[name]/bundle.js",
 	},
 	devtool: "source-map",
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as ReactDom from "react-dom"
 
 const App = () => {
-	return <div> A fluturecode web production </div>
+	return <div> A FlutureCode web production </div>
 }
 
 ReactDom.render(<App />, document.getElementById("root"))
